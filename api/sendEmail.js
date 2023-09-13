a611fc0d750401f94f5d8e930383cb47e5d5669f
Cody Schexnider <cody.schexnider@techngs.com>
2023-09-13 07:51:52 -0500

    fixed mailjet api client

---
+++ api/sendEmail.js
@@ -0,0 +1,44 @@
+// import { VercelRequest, VercelResponse } from "@vercel/node";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+import Mailjet from "node-mailjet";
+
+// const mailjet = new Mailjet({
+//   apiKey: "a1b59bc11a013bc02ba47b68875d64fa",
+//   apiSecret: "167e7a3c62d05ccba363058fb46991f4",
+// });
+
+const mailjet = Mailjet.apiConnect(
+  "a1b59bc11a013bc02ba47b68875d64fa",
+  "167e7a3c62d05ccba363058fb46991f4"
+);
+
+export default async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  const { name, email, message } = req.body;
+
+  try {
+    const result = await mailjet.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: { Email: "your@email.com", Name: "Your Name" },
+          To: [{ Email: "destination@email.com", Name: "Recipient Name" }],
+          Subject: `New message from ${name}`,
+          TextPart: `From: ${name}\nEmail: ${email}\n\n${message}`,
+        },
+      ],
+    });
+
+    return res.json({ success: true, data: result.body });
+  } catch (error) {
+    if (error instanceof Error) {
+      // Now TypeScript knows `error` is an instance of `Error`
+      console.error(error.message);
+    } else {
+      // Handle or log the error differently if it's not an instance of `Error`
+      console.error(error);
+    }
+  }
+};
