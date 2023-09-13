2e7326ecaa569fa478010f32470e26837f563b01
Cody Schexnider <cody.schexnider@techngs.com>
2023-09-12 19:28:33 -0500

    added vercel serverless function to call mailjet api

--- src/api/sendEmail.ts
+++ src/api/sendEmail.ts
@@ -1,58 +1,98 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { Buffer } from "buffer";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import { Buffer } from "buffer";
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//   if (req.method !== "POST") {
+//     return res.status(405).end();
+//   }
+
+//   const { name, email, message } = req.body as {
+//     name: string;
+//     email: string;
+//     message: string;
+//   };
+
+//   const mailjetData = {
+//     Messages: [
+//       {
+//         From: {
+//           Email: email, // Replace with the sender's email. Can be your business email.
+//           Name: name,
+//         },
+//         To: [
+//           {
+//             Email: "cody.schexnider@codesmith.io", // Replace with the email you want to forward to.
+//             Name: "Recipient Name", // Replace with the name of the recipient.
+//           },
+//         ],
+//         Subject: "New Contact Form Submission",
+//         TextPart: `From: ${name} <${email}>\n\n${message}`,
+//       },
+//     ],
+//   };
+
+//   try {
+//     const mailjetResponse = await fetch("https://api.mailjet.com/v3.1/send", {
+//       method: "POST",
+//       headers: {
+//         Authorization:
+//           "Basic " +
+//           Buffer.from(
+//             process.env.MAILJET_API_KEY + ":" + process.env.MAILJET_SECRET_KEY
+//           ).toString("base64"),
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(mailjetData),
+//     });
+
+//     if (!mailjetResponse.ok) {
+//       console.error("Mailjet error", await mailjetResponse.text());
+//       return res.status(500).end();
+//     }
+
+//     return res.status(200).end();
+//   } catch (error) {
+//     console.error("Sending email failed", error);
+//     return res.status(500).end();
+//   }
+// };
+
+// api/sendEmail.ts
+
+import { VercelRequest, VercelResponse } from "@vercel/node";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const mailjet = require("node-mailjet").connect(
+  process.env.MAILJET_API_KEY!,
+  process.env.MAILJET_SECRET_KEY!
+);
+
+export default async (req: VercelRequest, res: VercelResponse) => {
   if (req.method !== "POST") {
     return res.status(405).end();
   }
 
-  const { name, email, message } = req.body as {
-    name: string;
-    email: string;
-    message: string;
-  };
-
-  const mailjetData = {
-    Messages: [
-      {
-        From: {
-          Email: email, // Replace with the sender's email. Can be your business email.
-          Name: name,
-        },
-        To: [
-          {
-            Email: "cody.schexnider@codesmith.io", // Replace with the email you want to forward to.
-            Name: "Recipient Name", // Replace with the name of the recipient.
-          },
-        ],
-        Subject: "New Contact Form Submission",
-        TextPart: `From: ${name} <${email}>\n\n${message}`,
-      },
-    ],
-  };
+  const { name, email, message } = req.body;
 
   try {
-    const mailjetResponse = await fetch("https://api.mailjet.com/v3.1/send", {
-      method: "POST",
-      headers: {
-        Authorization:
-          "Basic " +
-          Buffer.from(
-            process.env.MAILJET_API_KEY + ":" + process.env.MAILJET_SECRET_KEY
-          ).toString("base64"),
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(mailjetData),
+    const result = await mailjet.post("send", { version: "v3.1" }).request({
+      Messages: [
+        {
+          From: { Email: "your@email.com", Name: "Your Name" },
+          To: [{ Email: "destination@email.com", Name: "Recipient Name" }],
+          Subject: `New message from ${name}`,
+          TextPart: `From: ${name}\nEmail: ${email}\n\n${message}`,
+        },
+      ],
     });
 
-    if (!mailjetResponse.ok) {
-      console.error("Mailjet error", await mailjetResponse.text());
-      return res.status(500).end();
-    }
-
-    return res.status(200).end();
+    return res.json({ success: true, data: result.body });
   } catch (error) {
-    console.error("Sending email failed", error);
-    return res.status(500).end();
+    if (error instanceof Error) {
+      // Now TypeScript knows `error` is an instance of `Error`
+      console.error(error.message);
+    } else {
+      // Handle or log the error differently if it's not an instance of `Error`
+      console.error(error);
+    }
   }
 };
